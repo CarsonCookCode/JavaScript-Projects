5c6e36c8a0d5d71bbc9bcf38c5c0f9ed24cfd9b2
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2021-10-18 12:47:10 -0700

    New files added.

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,6 @@
+var A = "These are \"words\" aren\'t they?", C = "...They are indeed."; //Declaring multiple variables and assigning string values.
+var D = A + C; //Declaring another variable and concatenating two strings.
+D = D.fontcolor("blue"); //Setting the property of fontcolor of D to blue.
+document.write(D); //Write D on the page.
+var B = 7 + 5; //Declaring a variable and performing an expression.
+window.alert(B); //Call alert method from window object with the value of B.
