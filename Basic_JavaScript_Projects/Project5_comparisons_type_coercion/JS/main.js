e85e9ae183d89588955e4d10bbb59f7399dc36da
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2021-10-20 13:47:13 -0700

    Changed

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,13 +1,19 @@
 //This determines the type of data of 'Bob' which is a string.
 document.write(typeof 'Bob');
+document.write('---')
 //This concatenates 10 and "10" despite the different data types (coercion).
 document.write(10 + "10")
+document.write('---')
 //This demonstrates in the console how changing the type of operator and
 //operand affects the result of the expression.
-console.log(10 == 10);
-console.log(10 == 9);
-console.log(10 === "10");
-console.log(10 == "10");
-console.log('-------------')
-console.log(2 > 1 || 1 > 5);
-console.log(2 > 1 && 5 > 1);
+document.write(10 == 10);
+document.write('---')
+document.write(10 == 9);
+document.write('---')
+document.write(10 === "10");
+document.write('---')
+document.write(10 == "10");
+document.write('---')
+document.write(2 > 1 || 1 > 5);
+document.write('---')
+document.write(2 > 1 && 5 > 1);
