a8064b76db98da09ce49c43ad70fd9ee4f1feeda
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2021-10-20 11:17:37 -0700

    New files added.

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,8 @@
+
+//This function will display the text "I am here" in the paragragh element.
+function displayText (x) {
+ //Here I set x equal to the element with the id "paragraph".   
+    x = document.getElementById("paragraph");
+//Here the text "I am here" on the page.
+    x.innerHTML += "I am here. "
+}
